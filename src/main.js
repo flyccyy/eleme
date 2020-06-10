e75c1883ac4ffdcc2a99bc55d7ccf1cb66c17e9d
lyfe <549222061@qq.com>
2020-06-10 18:48:00 +0800

    initial

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { Tab, Tabs } from 'vant';
 
 Vue.config.productionTip = false
+Vue.use(Tab).use(Tabs);
 
 new Vue({
   router,
---
+++ src/views/Home/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home-wrapper">
+    <div class="header"></div>
+    <van-tabs v-model="active">
+      <van-tab title="商品">内容 1</van-tab>
+      <van-tab title="评论">内容 2</van-tab>
+      <van-tab title="商家">内容 3</van-tab>
+    </van-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: 2
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
